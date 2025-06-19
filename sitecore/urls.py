523f7f8aa9eb8c98a13da1e0c6fa93e809429106
Bashar Arif <bashar.aarif@gmail.com>
2025-06-19 18:12:19 +0600

    fixed

--- sitecore/urls.py
+++ sitecore/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
-
+from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("pages.urls")),
 ]
+
+
+if settings.DEBUG and "debug_toolbar" in settings.INSTALLED_APPS:
+    import debug_toolbar
+
+    urlpatterns.append(path("__debug__/", include(debug_toolbar.urls)))
